1359fc18f602027ba7ac8e4b873a73db986d8ee6
hasibur rahman <shanto.ewu99@gmail.com>
2020-10-28 17:20:15 +0600

    init

--- hackerrank/nested-list.py
+++ hackerrank/nested-list.py
@@ -1,5 +1,6 @@
 #Sort a list according to the second element in sublist
-student =[['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]] 
+#student =[['Harry', 37.21], ['Berry', 37.20], ['Tina', 37.20], ['Akriti', 41], ['Harsh', 39], ['Aerry', 35.21]] 
+student = [['Harsh', 20],['Beria', 20],['Varun', 19],['Kakunami', 19],['Vikas', 21]]
 l = len(student) 
 result = []
 def Sort(student): 
@@ -14,16 +15,17 @@ def Sort(student):
 
 Sort(student) 
 print(student)
-print(student[0][1])
-print(student[0+1][1])
-print(student[0][0])
-print(student[1][0])
-print(student[0])
-print(student[1]) 
+# print(student[0][1])
+# print(student[0+1][1])
+# print(student[0][0])
+# print(student[1][0])
+# print(student[0])
+# print(student[1]) 
 
 for i in range(0, l): 
     if student[i][1] == student[1][1]:
         temp = student[i][0]
         result.append(temp)
 
-print(sorted(result))
+result.sort()
+print('\n'.join(map(str, result)))
